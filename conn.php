13f712ca1ee1fcc15c9b80dc38e5abfc3750e704
Jefferson Nunes <vitnunesjefferson@gmail.com>
2019-02-19 18:44:41 -0300

    colocando cadastro de usuarios

---
+++ conn.php
@@ -0,0 +1,12 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=projeto_integrador;charset=utf8mb4;port=3306';
+$db_user = 'root';
+$db_pass = '';
+
+try {
+    $db = new PDO($dsn, $db_user, $db_pass);
+}
+catch( PDOException $Exception ) {
+    echo $Exception->getMessage();
+}
